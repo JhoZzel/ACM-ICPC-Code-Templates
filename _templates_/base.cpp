4e5fc56074a6bb9d97a01121cd4c34ce03f0c5dc
JhoZzel <joel.lol177@gmail.com>
2023-10-22 22:29:12 -0500

    new template

---
+++ _templates_/base.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(0); cin.tie (0); cout.tie(0)
+#define dbg(x) do {cerr << #x <<" = " << (x) << endl; } while (false)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define eb emplace_back
+#define sz(x) ((int) x.size())
+#define F first
+#define S second
+using namespace std;
+using u128 = __uint128_t;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef priority_queue<int> pq_max;
+typedef priority_queue<int, vi, greater<int>> pq_min;
+
+const int N = 2e5 + 5;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+}
+
+int main() {
+    fast_io;
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
+
