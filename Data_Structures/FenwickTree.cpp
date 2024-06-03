5be0bfe039581d60288c2bafc45d4c0909b44cb8
JhoZzel <joel.lol177@gmail.com>
2024-06-03 00:28:56 -0500

    fenwick mika uwu

--- Data_Structures/FenwickTree.cpp
+++ Data_Structures/FenwickTree.cpp
@@ -1,60 +1,42 @@
 #include <bits/stdc++.h>
-#define fast_io ios_base::sync_with_stdio(false); cin.tie (NULL)
 using namespace std;
 typedef long long ll;
 
-struct FenwickTree { // 1-indexed
-    int n; 
-    vector<ll> ft;
+const int N = 1e5 + 5;
 
-    FenwickTree(const vector<int> &a) {
-        n = a.size();
-        ft = vector<ll>(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            update(i + 1, a[i]);
-        }
+struct FenwickTree {
+    ll t[N];
+    void update(int i, ll x) {
+        for (; i < N; i += i & -i) t[i] += x;
     }
-
-    void update(int pos, ll x) { // a[pos] += x
-        while(pos <= n) {
-            ft[pos] += x;
-            pos += (-pos) & pos;
-        }
+    ll sum(int i) {
+        ll sa = 0;
+        for (; i > 0; i -= i & -i) sa += t[i];
+        return sa;
     }
-    
-    ll query(int pos) { 
-        ll res = 0;
-        while(pos > 0) {
-            res += ft[pos];
-            pos &= pos - 1; // pos -= LSO(pos)
-        }
-        return res;
-    }
-
-    ll query(int l, int r) { 
-        return query(r) - query(l - 1);
-    }
-
-    ll at(int pos) { 
-        return query(pos) - query(pos - 1);
+    ll query(int l, int r) {
+        return sum(r) - sum(l - 1);
     }
 };
 
+FenwickTree FT; 
+
 int main() {
-    fast_io;
-    int n,m; 
-    cin >> n >> m;
-    vector<int> a(n);
-    for (int &e : a) cin >> e;
-    FenwickTree FT(a);
-    while(m--) {
+    int n,q;
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        int x; cin >> x;
+        FT.update(i, x);
+    }
+    while(q--) {
         int t,x,y;
         cin >> t >> x >> y;
-        if (t == 0) {
-            cout << FT.query(x,y) << "\n";
+        if (t) {
+            ll at = FT.query(x,x); 
+            FT.update(x, y - at);
         }
         else {
-            FT.update(x, y - FT.at(x));
+            cout << FT.query(x,y) << '\n';
         }
     }
     return 0;
