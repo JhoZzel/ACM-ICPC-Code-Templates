c2037082f3a14a33e599cbe73507966816388157
JhoZzel <joel.lol177@gmai.com>
2024-07-25 22:35:04 -0500

    dsu

---
+++ Data_Structures/DSU.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int N = 1e5 + 5;
+
+struct DSU {
+    int n;
+    vector<int> par;
+    vector<int> size;
+    DSU(int n) : par(n), size(n, 1) { 
+        iota(par.begin(), par.end(), 0); 
+    }
+    int get(int x) {
+        return x == par[x] ? x : par[x] = get(par[x]);
+    }
+    bool can(int a, int b) {
+        return get(a) != get(b);
+    }
+    void join(int a, int b) {
+        a = get(a), b = get(b);
+        if (a == b) return;
+        if (size[a] < size[b]) swap(a,b);
+        par[b] = a;
+        size[a] += size[b];
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n,m;
+    cin >> n >> m;
+    DSU dsu(n);
+    while(m--) {
+        int a,b;
+        string op;
+        cin >> op >> a >> b;
+        a--; b--;
+        if (op == "union") {
+            dsu.join(a,b);
+        }
+        else {
+            cout << (!dsu.can(a,b) ? "YES" : "NO") << '\n';
+        }
+    }
+    return 0;
+}
