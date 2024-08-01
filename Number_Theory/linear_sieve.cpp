28e1b70d8a6005e3f81824a749ffe1f970514a86
JhoZzel <joel.lol177@gmai.com>
2024-08-01 17:46:34 -0500

    sieve

--- Number_Theory/linear_sieve.cpp
+++ Number_Theory/linear_sieve.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 typedef long long ll;
 
-const int N = 1e8 + 1;
+const int MAX = 1e8 + 1;
 
-bitset<N> is_prime;
+bitset<MAX> is_prime;
 vector<int> primes;
 
 void sieve() {
     is_prime.set();
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i < N; i++) {
+    for (int i = 2; i < MAX; i++) {
 		if (is_prime[i]) primes.emplace_back(i);
 		for (int p : primes) {
-			if (i * p >= N) break;
+			if (i * p >= MAX) break;
 			is_prime[i * p] = false;
 			if (i % p == 0) break;
 		}
