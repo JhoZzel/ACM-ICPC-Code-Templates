a292e39944456778a34a5c3609a9ba39d1249558
JhoZzel <joel.lol177@gmail.com>
2025-05-31 18:56:01 -0500

    DP Divide and Conquer - Memory Improvement

--- Dynamic_Programming/dp_divide_and_conquer.cpp
+++ Dynamic_Programming/dp_divide_and_conquer.cpp
@@ -9,26 +9,28 @@ const int N = 3000 + 5;
 int n,K;
 int a[N];
 int pre[N];
-ll dp[N][N];
+ll dp[N][2];
 
+// DP Divide and Conquer:
 // opt(i - 1, k) <= opt(i, k)
+
 ll cost(int i, int j) { 
     ll sa = pre[j] - pre[i - 1];
     return sa * sa;
 }
 
-void go(int l, int r, int k, int opl = 1, int opr = n) {
+void go(int l, int r, int id, int opl = 1, int opr = n) {
     if (l > r) return;
     int mid = (l + r) / 2;
     pair<ll, int> mn = {INF, mid};
     for (int j = opl; j <= min(mid, opr); j++) {
-        mn = min(mn, pair{dp[j - 1][k - 1] + cost(j, mid), j});
+        mn = min(mn, pair{dp[j - 1][id ^ 1] + cost(j, mid), j});
     }
     int opt = mn.second;
-    dp[mid][k] = mn.first;
+    dp[mid][id] = mn.first;
 
-    go(l, mid - 1, k, opl, opt);
-    go(mid + 1, r, k, opt, opr);
+    go(l, mid - 1, id, opl, opt);
+    go(mid + 1, r, id, opt, opr);
 }
 
 int main() {
@@ -40,16 +42,14 @@ int main() {
         pre[i] = pre[i - 1] + a[i];
     }
     
-    for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) dp[i][j] = INF;
-
     dp[0][0] = 0;
+    for (int i = 1; i <= n; i++) dp[i][0] = INF;
 
-    for (int k = 1; k <= K; k++) go(1, n, k);
+    for (int k = 1; k <= K; k++) go(1, n, k & 1);
     
-    cout << dp[n][K] << "\n";
+    cout << dp[n][K & 1] << "\n";
 
     return 0;
 }
 
-// https://cses.fi/problemset/result/13125422/
-
+// https://cses.fi/problemset/result/13126191/
