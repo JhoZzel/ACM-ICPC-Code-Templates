2c49f654c326782bd39d6945899952abe776024a
JhoZzel <joel.lol177@gmail.com>
2025-06-15 22:33:59 -0500

    updating matrix struct

--- Data_Structures/Matrix_struct.cpp
+++ Data_Structures/Matrix_struct.cpp
@@ -1,57 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define dbg(x) cerr << #x << " = " << x << endl
-#define pv(x) cerr << #x << "[]: "; for (auto e : x) cerr << e << " "; cerr << endl
-#define raya cerr << string(20, '=') << endl
-
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define sz(x) (int)x.size()
-#define eb emplace_back
-#define ff first
-#define ss second
-
 using ll = long long;
 
 const int MOD = 1e9 + 7;
 
 struct Matrix {
-    int a[2][2] = {};
+    ll a[2][2];
 
-    Matrix() {}
-
-    void id() {
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                a[i][j] = (i == j);
-            }
-        }
+    Matrix() {
+        memset(a, 0, sizeof(a));
     }
 
-    Matrix operator+(const Matrix& B) {
+    void id() {
+        for (int i = 0; i < 2; i++) 
+            for (int j = 0; j < 2; j++) 
+                a[i][j] = (i == j);
+    }
+
+    ll* operator [] (int i) {
+        return a[i];
+    }
+
+    Matrix operator + (Matrix &B) {
         Matrix C;
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                C.a[i][j] = a[i][j] + B.a[i][j];
-                C.a[i][j] %= MOD;
+                C[i][j] = (a[i][j] + B[i][j]) % MOD;
             }
         }
         return C;
     }
-    Matrix operator*(const Matrix& B) {
+
+    Matrix operator * (Matrix &B) {
         Matrix C;
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 2; k++) {
-                    C.a[i][j] += 1ll * a[i][k] * B.a[k][j] % MOD;
-                    C.a[i][j] %= MOD;
+                    C[i][j] += a[i][k] * B[k][j] % MOD;
+                    C[i][j] %= MOD;
                 }
             }
         }
         return C;
     }
-    friend Matrix bpow(Matrix A, int e) {
+    
+    friend Matrix bpow(Matrix A, ll e) {
         Matrix R; R.id();
         while (e) {
             if (e & 1) R = R * A;
@@ -62,22 +56,27 @@ struct Matrix {
     }
 };
 
+void apply(Matrix M, ll &x, ll &y) {
+    ll nx = M[0][0] * x + M[0][1] * y;
+    ll ny = M[1][0] * x + M[1][1] * y;
+    x = nx % MOD;
+    y = ny % MOD;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     Matrix A;
-    A.a[0][0] = 1;
-    A.a[0][1] = 1;
-    A.a[1][0] = 1;
-    A.a[1][1] = 0;
-    
-    const int n = 10;
-    cout << "Fibonacci Numbers: ";
-    for (int i = 1; i < n; i++) {
-        Matrix r = bpow(A, i);
-        cout << r.a[0][1] << ' ';
-    }
-    cout << '\n';
+    A[0][0] = 1; A[0][1] = 1;
+    A[1][0] = 1; A[1][1] = 0;
+
+    ll n; cin >> n;
+    ll x = 1, y = 0;
+    apply(bpow(A, n), x, y);
+    cout << y << "\n";
+
+    return 0;
 }
+
+// https://cses.fi/problemset/result/13314961/
