301cd80126167cf06a65c5a93f4549c68c9d5111
JhoZzel <joel.lol177@gmail.com>
2025-08-11 09:29:32 -0500

    hld conmutative

--- Data_Structures/hld.cpp
+++ Data_Structures/hld.cpp
@@ -1,36 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 using ll = long long;
+ 
+struct node {
+    ll sum;
+    node() : sum(0) {}
+    node(int x) : sum(x) {}
+    node operator + (node R) {
+        node L = *this;
+        node q;
+        q.sum = L.sum + R.sum;
+        return q;
+    }
+};
 
-const int N = 1e5 + 5;
+
+const int N = 2e5 + 5;
 
 int n,q;
+int a[N];
+
+int h[N];
+int sz[N];
+int par[N];
+int head[N];
+
 int timer;
 int tin[N];
 int tout[N];
-int head[N];
-int par[N];
-int a[N];
-int h[N];
-int sz[N];
-int T[4 * N];
-int node[N];
-vector<tuple<int,int,int>> edges;
+
+node T[4 * N];
 vector<int> G[N];
+
 
 void pre_dfs(int u, int p = 0) {
     sz[u] = 1;
     if (p == G[u][0]) swap(G[u][0], G[u].back());
     for (int &v : G[u]) if (v != p) {
         pre_dfs(v, u);
-        sz[u] += sz[v];
+        sz[u] += v;
         if (sz[v] > sz[G[u][0]]) {
             swap(v, G[u][0]);
         }
     }
 }
-
+ 
 void dfs(int u, int p = 0) {
     tin[u] = timer++;
     for (int v : G[u]) if (v != p) {
@@ -41,107 +56,71 @@ void dfs(int u, int p = 0) {
     }
     tout[u] = timer - 1;
 }
-
-void build(int id = 1, int tl = 0, int tr = n - 1) {
-    if (tl == tr) T[id] = a[tl];
-    else {
-        int tm = (tl + tr) / 2;
-        build(2 * id, tl, tm);
-        build(2 * id + 1, tm + 1, tr);
-        T[id] = max(T[2 * id], T[2 * id + 1]);
-    }
-}
-
+ 
 void update(int pos, int x, int id = 1, int tl = 0, int tr = n - 1) {
     if (tl == tr) T[id] = x;
     else {
         int tm = (tl + tr) / 2;
-        if (pos <= tm) update(pos, x, 2 * id, tl, tm);
-        else update(pos, x, 2 * id + 1, tm + 1, tr);
-        T[id] = max(T[2 * id], T[2 * id + 1]);
+        if (pos <= tm) 
+            update(pos, x, 2 * id, tl, tm);
+        else
+            update(pos, x, 2 * id + 1, tm + 1, tr);
+ 
+        T[id] = T[2 * id] + T[2 * id + 1];
     }
 }
-
-int query(int l, int r, int id = 1, int tl = 0, int tr = n - 1) {
-    if (l > r) return 0;
+ 
+node query(int l, int r, int id = 1, int tl = 0, int tr = n - 1) {
+    if (l > r) return node();
     if (tl == l and tr == r) return T[id];
     int tm = (tl + tr) / 2;
-    int valL = query(l, min(r, tm), 2 * id, tl, tm);
-    int valR = query(max(l, tm + 1), r, 2 * id + 1, tm + 1, tr);
-    return max(valL, valR);
+    node valL = query(l, min(r, tm), 2 * id, tl, tm);
+    node valR = query(max(l, tm + 1), r, 2 * id + 1, tm + 1, tr);
+    return valL + valR;
 }
-
-int query_hld(int u, int v) { // conmutative
+ 
+node query_hld(int u, int v) { // conmutative
     if (h[u] > h[v]) swap(u, v);
     int x = head[u], y = head[v];
-    if (x == y) {
-        if (u == v) return 0;
-        assert(tin[u] + 1 <= tin[v]);
-        return query(tin[u] + 1, tin[v]);
-    }
+    if (x == y) return query(tin[u], tin[v]);
     if (h[x] < h[y]) swap(x, y), swap(u, v);
-    return max(query(tin[x], tin[u]), query_hld(par[x], v));
+    return query(tin[x], tin[u]) + query_hld(par[x], v);
 }
-
-void clean() {
-    for (int i = 0; i < n; i++) {
-        G[i].clear();
-    }
-    edges.clear();
-    timer = 0;
-}
-
-void solve() {
-    pre_dfs(0);
-    dfs(0);
-
-    for (int i = 0; i < n - 1; i++)  {
-        auto [u, v, w] = edges[i];
-        if (u == par[v]) swap(u, v);
-        a[tin[u]] = w;
-        node[i] = u;
-    } 
-
-    build();
-
-    string op; 
-    while(cin >> op) {
-        if (op == "QUERY") {
-            int u,v;
-            cin >> u >> v;
-            u--; v--;
-            cout << query_hld(u, v) << "\n";
-        } else if (op == "CHANGE") {
-            int i,x;
-            cin >> i >> x;
-            i--;
-            int u = node[i];
-            update(tin[u], x);
-        } else {
-            assert(op == "DONE");
-            break;
-        }
-    }
-
-    clean();
-}
-
+ 
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
-
-    int t; cin >> t;
-    while(t--) {   
-        cin >> n;
-        for (int i = 1; i < n; i++) {
-            int u,v,w;
-            cin >> u >> v >> w;
-            u--; v--;
-            G[u].emplace_back(v);
-            G[v].emplace_back(u);
-            edges.emplace_back(u, v, w);
-        }
-        solve();
-    }
     
+    cin >> n >> q;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 1; i < n; i++) {
+        int u,v;
+        cin >> u >> v;
+        u--; v--;
+        G[u].emplace_back(v);
+        G[v].emplace_back(u);
+    }
+ 
+    pre_dfs(0);
+    dfs(0);
+    
+    for (int i = 0; i < n; i++) {
+        update(tin[i], a[i]);
+    }
+ 
+    while(q--) {
+        int op; cin >> op;
+        if (op == 1) {
+            int u,x;
+            cin >> u >> x;
+            u--;
+            update(tin[u], x);
+        } else {
+            int v;
+            cin >> v;
+            v--;
+            cout << query_hld(0, v).sum << "\n"; // path (0, v)
+        }
+    }
+ 
     return 0;
 }
