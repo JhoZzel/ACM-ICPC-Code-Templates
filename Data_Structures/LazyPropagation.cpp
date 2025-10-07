1940161901e81278ca065556c7c0597c850c53da
JhoZzel <joel.lol177@gmail.com>
2025-10-06 21:03:01 -0500

    lazy segment tree

--- Data_Structures/LazyPropagation.cpp
+++ Data_Structures/LazyPropagation.cpp
@@ -1,94 +1,98 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define dbg(x) cerr << #x << " = " << x << endl
-#define pv(x) cerr << #x << "[]: "; for (auto e : x) cerr << e << " "; cerr << endl
-#define raya cerr << string(20, '=') << endl
-
-#define eb emplace_back
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define sz(x) (int)x.size()
-#define ff first
-#define ss second
-
+ 
 using ll = long long;
+ 
+const int N = 2e5 + 5;
 
-const int N = 1e5 + 5;
+struct line {
+    int m;
+    ll b;
+    line() : m(1), b(0) {}
+    line(int m, ll b) : m(m), b(b) {}
+    line operator + (line other) {
+        line q;
+        q.m = m * other.m;
+        q.b = b * other.m + other.b;
+        return q;
+    }
+};
 
 int n,q;
+int a[N];
 ll T[4 * N];
-ll lazy[4 * N];
-
-// id => Node's idx in the tree 1-indexed
-// [tl, tr] => Range of the node || root: [1, n]
-
+bool marked[4 * N];
+line lazy[4 * N];
+ 
+void build(int id = 1, int tl = 1, int tr = n) {
+    if (tl == tr) T[id] = a[tl];
+    else {
+        int tm = (tl + tr) / 2;
+        build(2 * id, tl, tm);
+        build(2 * id + 1, tm + 1, tr);
+        T[id] = T[2 * id] + T[2 * id + 1];
+    }
+}
+ 
 void push(int id, int tl, int tr) {
-    T[id] += 1ll * (tr - tl + 1) * lazy[id];
+    if (!marked[id]) return;
+    T[id] = T[id] * lazy[id].m + (tr - tl + 1) * lazy[id].b;
     if (tl != tr) {
-        lazy[2 * id] += lazy[id];
-        lazy[2 * id + 1] += lazy[id];
+        lazy[2 * id] = lazy[2 * id] + lazy[id];
+        lazy[2 * id + 1] = lazy[2 * id + 1] + lazy[id];
+        marked[2 * id] = marked[2 * id + 1] = 1;
     }
-    lazy[id] = 0;
+    lazy[id] = line();
+    marked[id] = 0;
 }
-
-void update(int l, int r, int x, int id = 1, int tl = 1, int tr = n) {
-    if (lazy[id]) push(id, tl, tr);
+ 
+void update(int l, int r, line x, int id = 1, int tl = 1, int tr = n) {
+    push(id, tl, tr);
     if (l > r) return;
-    if (l == tl and r == tr) {
-        lazy[id] += x;
+    if (tl == l and tr == r) {
+        lazy[id] = lazy[id] + x;
+        marked[id] = 1;
         push(id, tl, tr);
-        return;
+    } else {
+        int tm = (tl + tr) / 2;
+        update(l, min(r, tm), x, 2 * id, tl, tm);
+        update(max(tm + 1, l), r, x, 2 * id + 1, tm + 1, tr);
+        T[id] = T[2 * id] + T[2 * id + 1];
     }
-    int tm = (tl + tr) / 2;
-    update(l, min(r, tm), x, 2 * id, tl, tm);
-    update(max(l, tm + 1), r , x, 2 * id + 1, tm + 1, tr);
-    T[id] = T[2 * id] + T[2 * id + 1];
 }
-
+ 
 ll query(int l, int r, int id = 1, int tl = 1, int tr = n) {
-    if (lazy[id]) push(id, tl, tr);
+    push(id, tl, tr);
     if (l > r) return 0ll;
-    if (l == tl and r == tr) return T[id];
+    if (tl == l and tr == r) return T[id];
     int tm = (tl + tr) / 2;
-    ll valL = query(l, min(r, tm), 2 * id, tl, tm);
-    ll valR = query(max(l, tm + 1), r , 2 * id + 1, tm + 1, tr);
+    ll valL = query(l, min(tm, r), 2 * id, tl, tm);
+    ll valR = query(max(tm + 1, l), r, 2 * id + 1, tm + 1, tr);
     return valL + valR;
 }
+ 
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
 
-void clean(int id = 1, int tl = 1, int tr = n) {
-    T[id] = lazy[id] = 0;
-    if (tl == tr) return;
-    int tm = (tl + tr) / 2;
-    clean(2 * id, tl, tm);
-    clean(2 * id + 1, tm + 1, tr);
-}
-
-void solve() {
     cin >> n >> q;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    build();
+
     while(q--) {
         int op; cin >> op;
-        if (op == 0) {
+        if (op != 3) {
             int l,r,x;
             cin >> l >> r >> x;
-            update(l, r, x);
-        }
-        else {
+            update(l, r, line(op == 1, x));
+        } else {
             int l,r;
             cin >> l >> r;
             cout << query(l, r) << '\n';
         }
     }
-    clean();
-}
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); 
-    int t; cin >> t;
-    while(t--) {
-        solve();
-    }
     return 0;
 }
 
+// https://cses.fi/problemset/result/14825128/
