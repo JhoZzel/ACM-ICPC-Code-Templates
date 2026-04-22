1ef055c137282d4c3801b379869790c53a6127e0
JhoZzel <joel.lol177@gmail.com>
2026-04-22 13:15:25 -0500

    some modifications in trie

--- Strings/trie.cpp
+++ Strings/trie.cpp
@@ -1,21 +1,23 @@
+// Trie
+//
+
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
+using ll = long long;
 
-const int LOG = 60;
+const int LOG = 61;
 const int N = 1e5 + 5;  
 
 struct Trie{ 
     int nodes = 0;
     vector<vector<int>> trie;
 
-    Trie() : trie(N * LOG, vector<int>(2, 0)) {}
     Trie(int n) : trie((n + 5) * LOG, vector<int>(2, 0)) {}
 
     void add(ll x) { // Insert X in the trie
         int r = 0;
-        for (int i = LOG; i >= 0; i--) {
+        for (int i = LOG - 1; i >= 0; i--) {
             int ch = (x >> i) & 1;
             if (!trie[r][ch]) trie[r][ch] = ++nodes;
             r = trie[r][ch];
@@ -25,11 +27,11 @@ struct Trie{
     ll query(ll x) { // max(X XOR Y) when Y is in the trie
         ll ans = 0;
         int r = 0;
-        for (int i = LOG; i >= 0; i--) {
+        for (int i = LOG - 1; i >= 0; i--) {
             int ch = (x >> i) & 1;
-            if (trie[r][1 - ch]) {
+            if (trie[r][ch ^ 1]) {
                 ans += (1ll << i);
-                r = trie[r][1 - ch];
+                r = trie[r][ch ^ 1];
             } 
             else r = trie[r][ch];
         }
@@ -38,16 +40,16 @@ struct Trie{
 };
 
 int main() {
-    Trie t;
-    t.add(0);
-    t.add(5);
-    t.add(15);
-    t.add(7);
-    
-    ll x;
-    while(cin >> x) {
-        cout << t.query(x) << endl;
-    }
-    
+    cin.tie(0) -> sync_with_stdio(0);
+    int n; cin >> n;
+
+    Trie t(n);
+    t.add(1632);
+    t.add(1124);
+    t.add(443);
+    t.add(2121);
+
+    cout << t.query(2003) << '\n';
+        
     return 0;
 }
