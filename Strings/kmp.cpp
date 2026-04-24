c876337dacde43d2160cccc3f9427dfff7e8df86
JhoZzel <joel.lol177@gmail.com>
2026-04-24 14:58:28 -0500

    even: center [i-1,i]

--- Strings/kmp.cpp
+++ Strings/kmp.cpp
@@ -1,15 +1,15 @@
 // KMP
 //
 
-vector<int> LPS(string &s) { 
-    const int n = s.size();
+// Para cada i, lps[i] es la longitud máxima de un prefijo propio (no puede tomar todo) que también es sufijo del substring s[0..i].
+
+vector<int> kmp(string &s) { 
+    int n = s.size();
     vector<int> lps(n);
     for (int i = 1; i < n; i++) {
         int j = lps[i - 1];
-        while (j > 0 and s[i] != s[j])
-            j = lps[j - 1];
-        if (s[i] == s[j])
-            j++;
+        while (j > 0 and s[i] != s[j]) j = lps[j - 1];
+        if (s[i] == s[j]) j++;
         lps[i] = j;
     }
     return lps;
--- Strings/manacher.h
+++ Strings/manacher.h
@@ -1,8 +1,9 @@
 // Manacher
 //
 
-void manacher(vector<int> &s, vector<int> &d1, vector<int> &d2) {
+void manacher(string &s, vector<int> &d1, vector<int> &d2) {
     int n = s.size();
+
     d1.assign(n, 0);
     for (int i = 0, l = 0, r = -1; i < n; i++) {
         int k = (i > r) ? 1 : min(d1[l + r - i], r - i + 1);
@@ -10,11 +11,12 @@ void manacher(vector<int> &s, vector<int> &d1, vector<int> &d2) {
         d1[i] = k--;
         if (i + k > r) l = i - k, r = i + k;
     }
-    d2.assign(n, 0); 
-    for (int i = 0, l = 0, r = -1; i < n; i++) { // [i, i + 1]
-        int k = (i > r) ? 0 : min(d2[l + r - i - 1], r - i);
-        while (i - k >= 0 and i + 1 + k < n and s[i - k] == s[i + 1 + k]) k++;
+
+    d2.assign(n, 0);
+    for (int i = 0, l = 0, r = -1; i < n; i++) { // [i - 1, i]
+        int k = (i > r) ? 0 : min(d2[l + r - i + 1], r - i + 1);
+        while (i - k - 1 >= 0 and i + k < n and s[i - k - 1] == s[i + k]) k++;
         d2[i] = k--;
-        if (i + 1 + k > r) l = i - k, r = i + k + 1;
+        if (i + k > r) l = i - k - 1, r = i + k;
     }
 }
